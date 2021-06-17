11aef704aa44c337c41d594d8d630d918ea3d6b5
Alex Felipe <alexfelipevieira@gmail.com>
2021-06-17 10:30:09 -0300

    1.4

--- app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -1,19 +1,26 @@
 package br.com.alura.orgs.dao
 
 import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
-    fun adiciona(produto: Produto){
+    fun adiciona(produto: Produto) {
         produtos.add(produto)
     }
 
-    fun buscaTodos() : List<Produto> {
+    fun buscaTodos(): List<Produto> {
         return produtos.toList()
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de frutas",
+                descricao = "Laranja, maçãs e uva",
+                valor = BigDecimal("19.83")
+            )
+        )
     }
 
 }
