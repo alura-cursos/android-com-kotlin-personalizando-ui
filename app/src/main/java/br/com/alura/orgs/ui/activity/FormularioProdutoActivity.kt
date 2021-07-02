56a601bfb2403224285516964bbf342d550e2256
Alex Felipe <alexfelipevieira@gmail.com>
2021-07-02 10:56:31 -0300

    5.5

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -23,6 +23,7 @@ class FormularioProdutoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        title = "Cadastrar produto"
         configuraBotaoSalvar()
         binding.activityFormularioProdutoImagem.setOnClickListener {
             FormularioImagemDialog(this)
